2e33d7ca59b8f45cda2694d91b4ba73842ba688c
Ryan Johnson <johnson2500@live.com>
2018-04-11 22:19:58 -0700

    Adding Messages Components
    Adding Messageing Component for messaging page

--- src/Pages/Auth/Auth.js
+++ src/Pages/Auth/Auth.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
+import SignInWidget from './SignIn/signInWidget';
+import SignUpWidget from './SignUp/SignUpWidget';
+
 export default class Auth extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      authType : <SignInWidget firebase={ this.props.firebase } saveFireBase = { this.saveFireBase.bind(this) }/>
+    }
+  }
+
   render(){
     return(
       <div>
-        
+        { this.state.authType }
       </div>
     )
   }
---
+++ src/Pages/Messages/Messages.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './Messages.css'
+
+export default class Messages extends Component{
+  render(){
+    return(
+      <div></div>
+    )
+  }
+}
